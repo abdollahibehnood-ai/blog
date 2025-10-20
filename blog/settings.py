fd2488338372893c283d008fa1ced3db1a18dd80
Behnood Abdollahi <abdollahibehnood@gmail.com>
2025-10-20 13:19:37 +0330

    Add pagination

--- blog/settings.py
+++ blog/settings.py
@@ -134,9 +134,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 30,
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
 }
 
 SIMPLE_JWT = {
